23a790bded1299b4788efd8a29bc85073383eeb8
sky cherfa <sherifcherfa@gmail.com>
2022-12-22 15:50:31 -0500

    addresses and ABI

---
+++ constants/addresses.ts
@@ -0,0 +1,2 @@
+export const supertoken_factory_proxy: string = '0x2C90719f25B10Fc5646c82DA3240C76Fa5BcCF34';
+export const supertoken_factory: string = '0x092462ef87bDd081a6346102B0Be134ff63da01B';
