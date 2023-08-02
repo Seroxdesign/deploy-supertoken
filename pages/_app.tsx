3da208c820f495a1f4da48d82a88d7294717138e
Sero <69639595+Seroxdesign@users.noreply.github.com>
2023-08-02 11:36:07 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,30 @@ import { Layout } from '../components/layout';
 import { WagmiConfig, createClient } from "wagmi";
 import { ConnectKitProvider, getDefaultClient } from "connectkit";
 import { mainnet, polygon, optimism, arbitrum, gnosis, avalanche, bsc, arbitrumGoerli, goerli, polygonMumbai, optimismGoerli, avalancheFuji } from "wagmi/chains";
+import { Chain } from '@wagmi/core'
+ 
+export const base = {
+  id: 8453,
+  name: "base",
+  network: "Base Mainnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: { http: ['https://mainnet.base.org'] },
+    default: { http: ['https://mainnet.base.org'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'BaseScan', url: 'https://basescan.org' },
+    default: { name: 'BaseScan', url: 'https://basescan.org' },
+  },
+} as Chain
 
 const alchemyId = process.env.ALCHEMY_ID;
 
-const chains = [mainnet, polygon, optimism, arbitrum, gnosis, avalanche, bsc, arbitrumGoerli, goerli, polygonMumbai, optimismGoerli, avalancheFuji];
+const chains = [mainnet, polygon, optimism, arbitrum, gnosis, avalanche, bsc, arbitrumGoerli, goerli, polygonMumbai, optimismGoerli, avalancheFuji, base];
 
 const client = createClient(
   getDefaultClient({
